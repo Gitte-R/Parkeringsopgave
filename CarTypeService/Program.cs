3c79735fa1b327239e48584ca17784807f078066
Gitte-R <gittepige@gmail.com>
2023-09-25 21:36:42 +0200

    Updates

--- CarTypeService/Program.cs
+++ CarTypeService/Program.cs
@@ -1,5 +1,6 @@
 using CarTypeService.Services;
 using Polly;
+using Scrutor;
 using System.Net.Http;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,17 +9,19 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddSingleton<IConfiguration>(builder.Configuration);
 
-//builder.Services.AddScoped<IMotorApiService, MotorApiService>();
-//builder.Services.AddHttpClient<IMotorApiService, MotorApiService>();
-
 builder.Services.AddHttpClient<IMotorApiService, MotorApiService>().AddTransientHttpErrorPolicy(p =>
 p.WaitAndRetryAsync(3, attempt => TimeSpan.FromMilliseconds(100 * Math.Pow(2, attempt))));
+
+builder.Services.Scan(selector => selector.FromAssemblyOf<IMotorApiService>().AddClasses(classes => classes.AssignableTo<IMotorApiService>()).AsImplementedInterfaces());
 
 var MotorApiKey = builder.Configuration["Parking:MotorApiKey"];
 var app = builder.Build();
